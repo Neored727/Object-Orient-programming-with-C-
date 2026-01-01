e4720d557463644b4e6e4c1841ef5405c1d3f6cb
Neo RED <Neored727@gmail.com>
2026-01-01 17:05:01 +0530

    Add files via upload

---
+++ Assets/Mod4_Workshop.cpp
@@ -0,0 +1,79 @@
+//M0de 4 : workshops  
+#include <iostream>
+#include <string>
+using namespace std;
+//4.01Hellow simple
+
+//void printHI(int Num);
+//
+//int main()
+//{
+//	int Loop_Amount; // loop by client
+//	cout << "enter the number of times for the loop: ";
+//	cin >> Loop_Amount;
+//
+//	printHI(Loop_Amount);
+//	return 0;
+//
+//
+//}
+//
+//
+////4.02 Hello simple input 
+//
+//void printHI(int Num)
+//{
+//	int x;
+//	for (x = 0; x < Num; x++)
+//	{
+//		cout << "Hello" << endl;
+//	}
+//
+//}
+
+// 4.03 Simple output 
+
+//int printHelloAgain();
+//
+//int main()
+//{
+//	
+//	string Answer;
+//	int Number_count;
+//	cout << "Do you want to build a snowman [Y/N] : ";
+//	getline(cin, Answer);
+//	
+//	if (Answer == "Y") { Number_count = printHelloAgain(); }
+//	else {
+//		if (Answer == "N") { cout << "Thank u for nothing!!" << endl; }
+//		else { cout << "Invalid answer stupid" << endl; }
+//	}
+//	cout << "Count = " << Number_count << endl;
+//	return 0;
+//
+//
+//}
+//
+//
+////4.02 Hello simple input 
+//
+//int printHelloAgain()
+//{
+//
+//	int count = 0; //Hello print count , starting with 0 considering the no counts situation
+//	bool yesorNo = false;
+//
+//
+//	cout << "Do u wanna play (1 or 0): ";
+//	cin >> yesorNo;
+//
+//	while (yesorNo) 
+//	{
+//		cout << "Hello !!!!!" << endl;
+//		count++;
+//		cout << "Do u wanna play again (1 or 0): ";
+//		cin >> yesorNo;
+//	}
+//
+//	return count;
+//}
